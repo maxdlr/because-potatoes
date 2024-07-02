89125ac8945a681bf936036e5cd01df5715fadd1
maxdlr <contact@maxdlr.com>
2024-07-02 18:45:09 +0200

    feat: CardIdentifierEnum

---
+++ src/Enum/CardIdentifierEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum;
+
+enum CardIdentifierEnum: int
+{
+    case POTATO = 1;
+    case FRIE = 2;
+    case VITELOTTE = 3;
+    case CRISP = 4;
+    case CROWN = 5;
+    case UNDERWEAR = 6;
+    case GLOVES = 7;
+}
