0326460546fa509d63af2535addd11c879bd5c5c
GT <93249177+Gab1TN@users.noreply.github.com>
2024-07-03 14:30:28 +0200

    Card.js

---
+++ public/assets/scripts/Class/Card.js
@@ -0,0 +1,65 @@
+"use strict";
+import FetchManager from "../Service/FetchManager.js";
+
+class Card {
+  id = null;
+  potatoe = "";
+  gloves = false;
+  underwear = false;
+  crown = false;
+  imageUrl = "";
+  identifier = "";
+
+  constructor(identifier = 0) {
+    this.identifier = identifier;
+    this.mountCard();
+  }
+
+  async addToStack(stackId = 0) {
+    const data = {
+      stackId: stackId,
+      id: this.id,
+    };
+
+    const response = await FetchManager.post(
+      "/add-card-to-stack/" + stackId,
+      data
+    );
+    this.id = response.card.id;
+
+    return response.message;
+  }
+
+  static async getCardById(id) {
+    const response = await FetchManager.get("/card/" + id);
+    return Card.fromData(response);
+  }
+
+  static async getAllCardsByGameId(gameId) {
+    const response = await FetchManager.get("/cards/" + gameId);
+    return response.map((data) => Card.fromData(data));
+  }
+
+  /**
+   * @returns void
+   */
+  static async mountCard() {
+    const url = "/card/"; // We get the card informations by the identifier
+    const card = FetchManager.get(url + this.identifier);
+
+    this.id = card.id;
+    this.potatoe = card.potatoe;
+    this.this.underwear = card.underwear;
+    this.gloves = card.gloves;
+    this.crown = card.crown;
+    this.imageUrl = card.imageUrl;
+    this.identifier = card.identifier;
+
+    return this;
+  }
+}
+
+/**
+ * Set class accessible from anywhere
+ */
+export default Card;
