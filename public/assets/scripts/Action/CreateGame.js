ca3c195deaee67ebb52759bfbbf24895c19ae2a6
maxdlr <contact@maxdlr.com>
2024-07-03 16:56:03 +0200

    feat create game with creator as new player and redirect to lobby

--- public/assets/scripts/Action/CreateGame.js
+++ public/assets/scripts/Action/CreateGame.js
@@ -1,17 +1,58 @@
 'use strict';
-console.log(window.location.pathname)
-const createGameFormUrl = '/create-game-form';
+import Game from "../Class/Game.js";
+import Player from "../Class/Player.js";
 
-const username = document.querySelector("[name='username']")
-const formData = {};
+const usernameInput = document.querySelector("[name='username']");
+const birthdayInput = document.querySelector("[name='birthday']");
+const submitBtn = document.getElementById('createGameSubmit');
 
-if (window.location.pathname === '/') {
-    const createBtn = document.getElementById('create-game');
-    createBtn.addEventListener('click', () => {
-        window.location.replace(createGameFormUrl);
+function hideSubmit() {
+    if (!submitBtn.classList.contains('d-none')) {
+        submitBtn.classList.add('d-none')
+    }
+}
+function showSubmit() {
+    if (submitBtn.classList.contains('d-none')) {
+        submitBtn.classList.remove('d-none')
+    }
+}
+function isFormValid() {
+    return usernameInput.value !== '' && birthdayInput.value !== ''
+}
+
+function checkIsSubmittable() {
+    for (const formEl of [usernameInput, birthdayInput]) {
+        formEl.addEventListener('input', () => {
+            if (isFormValid()) {
+                showSubmit();
+            }
+        })
+    }
+}
+
+async function createGame() {
+    const game = new Game();
+    const player = new Player(
+        usernameInput.value,
+        birthdayInput.value
+    );
+    const newGame = await game.create();
+    const newPlayer = await player.addToGame(newGame.id)
+
+    if (newPlayer) {
+        return window.location.replace('/lobby');
+    }
+}
+
+function submit() {
+    submitBtn.addEventListener('click', async () => {
+        console.log('submitted')
+        await createGame();
     })
 }
 
+hideSubmit();
+checkIsSubmittable();
+submit();
 
-console.log(username)
 
